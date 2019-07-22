f2906a184f6d76e8d52171b7a88c6a5fef4d8337
drnesr <drnesr@gmail.com>
2019-07-23 02:19:51 +0300

    Starting Fasting times analysis

--- JupyterHere.bat
+++ JupyterHere.bat
@@ -7,7 +7,7 @@ REM # If you need special Anaconda environment
 REM # Uncomment the following line
 REM call activate spark_env
 
-call jupyter nbextension enable --py nbzip
+REM call jupyter nbextension enable --py nbzip
 REM # It is important to add the 'call' 
 call jupyter notebook
 REM # The pause command is to keep the command window active.
